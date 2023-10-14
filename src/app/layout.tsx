eb49d763790de28feddc04eedfc0241e55e3c61a
Isaac <isaachkdeveloper@gmail.com>
2023-10-15 01:21:03 +0800

    chores: Rename

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster } from '@/components/ui/toaster';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Demo System',
+  title: 'SketchBlend',
   description: 'This is a demo application',
 };
 
