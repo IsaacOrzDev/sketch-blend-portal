5080761485f2a15f981271256bfba378c2d4b384
Isaac <isaachkdeveloper@gmail.com>
2023-10-18 00:29:42 +0800

    feat: Update styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { Github } from 'lucide-react';
 export default function Footer() {
   return (
     <>
-      <div className="w-full h-20 fixed bottom-0 left-0 z-30 flex justify-center items-center max-lg:px-4 bg-background border-b border-black">
+      <div className="w-full h-20 fixed bottom-0 left-0 z-30 flex justify-center items-center max-xl:px-4 bg-background border-b border-black">
         <div className="w-full flex justify-between lg:max-w-6xl">
           <p className="text text-sm text-outline">
             This is a open-source demo application, built with microservices
