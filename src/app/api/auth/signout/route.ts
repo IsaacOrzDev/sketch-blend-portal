e8540703a5e83c1aa438a112ac59369db0a0d9b6
Isaac <isaachkdeveloper@gmail.com>
2023-10-18 16:40:14 +0800

    fix: Cannot delete cookies

--- src/app/api/auth/signout/route.ts
+++ src/app/api/auth/signout/route.ts
@@ -4,8 +4,19 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(request: NextRequest) {
   const redirectUrl = `${process.env.NEXT_PUBLIC_BASE_URL}`;
   const response = NextResponse.redirect(redirectUrl);
-  // response.cookies.delete(COOKIES_CONFIG.ACCESS_TOKEN_KEY);
-  response.cookies.set(COOKIES_CONFIG.ACCESS_TOKEN_KEY, '');
+
+  if (process.env.NODE_ENV === 'production') {
+    const domain = (process.env.NEXT_PUBLIC_API_BASE_URL ?? '')
+      .split('.')
+      .slice(1)
+      .join('.');
+    response.cookies.delete({
+      domain,
+      name: COOKIES_CONFIG.ACCESS_TOKEN_KEY,
+    });
+  } else {
+    response.cookies.delete(COOKIES_CONFIG.ACCESS_TOKEN_KEY);
+  }
 
   return response;
 }
