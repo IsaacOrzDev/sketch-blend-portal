dd007695f7de644a0110619fb2d130a9ebcd039a
Isaac <isaachkdeveloper@gmail.com>
2023-10-20 10:22:44 +0800

    feat: Update styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,8 +10,22 @@ export default function Footer(props: Props) {
       <div className="w-full h-20 fixed bottom-0 left-0 z-30 flex justify-center items-center max-xl:px-4 bg-background border-b border-black">
         <div className="w-full flex justify-between items-center lg:max-w-6xl">
           <p className="text text-sm">
-            This is a open-source demo application, the backend is built with
-            microservices architecture and host in Kubnernetes with AWS.
+            This is a demo application, the backend is built with{' '}
+            <a
+              href="https://aws.amazon.com/microservices"
+              className="underline"
+            >
+              micro-services architecture
+            </a>{' '}
+            and host in{' '}
+            <a href="https://kubernetes.io/" className="underline">
+              Kubnernetes
+            </a>{' '}
+            with{' '}
+            <a href="https://aws.amazon.com/" className="underline">
+              AWS
+            </a>
+            .
           </p>
           <a href={process.env.NEXT_PUBLIC_GITHUB_PROJECT_URL}>
             <Github />
