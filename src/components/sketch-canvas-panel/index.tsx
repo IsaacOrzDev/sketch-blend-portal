59918b0d0e9ae9bf1e35422c379631dbe81ea0cb
Isaac <isaachkdeveloper@gmail.com>
2023-10-22 17:31:14 +0800

    feat: Update styling

--- src/components/sketch-canvas-panel/index.tsx
+++ src/components/sketch-canvas-panel/index.tsx
@@ -143,7 +143,7 @@ export default function SketchCanvasPanel(props: Props) {
         strokeWidth={strokeWidth}
         strokeColor={theme === 'light' ? 'black' : 'white'}
       />
-      <div className="absolute top-0 left-0 w-full py-2 px-4 flex justify-between items-center">
+      <div className="fixed top-[100px] left-0 w-full py-2 px-4 flex justify-between items-center">
         <div className="flex gap-x-2">
           <Button
             variant="outline"
