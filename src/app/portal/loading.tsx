58a612323368eddafdceba537da72ef414903258
Isaac <isaachkdeveloper@gmail.com>
2023-10-23 22:16:07 +0800

    feat: Update styling

--- src/app/portal/loading.tsx
+++ src/app/portal/loading.tsx
@@ -1,9 +1,3 @@
-import Header from '@/components/header';
-
 export default function Loading() {
-  return (
-    <div className="w-full h-screen flex">
-      <Header onlyLogo />
-    </div>
-  );
+  return <div className="w-full h-screen flex"></div>;
 }
