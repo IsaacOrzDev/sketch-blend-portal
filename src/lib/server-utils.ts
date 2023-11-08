ffdf5ff352c25e1a1547d36ca14c27a54a6694d7
Isaac <isaachkdeveloper@gmail.com>
2023-11-08 18:41:38 +0800

    feat: Update server utils

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -4,6 +4,12 @@ export const isClusterExist = () => {
 
 export const getBaseUrl = () => {
   return isClusterExist()
-    ? process.env.NEXT_PUBLIC_BASE_URL
+    ? process.env.NEXT_PUBLIC_API_BASE_URL
     : process.env.NEXT_PUBLIC_TEMPORARY_API_BASE_URL;
+};
+
+export const getDocUrl = () => {
+  return isClusterExist()
+    ? process.env.NEXT_PUBLIC_DOCUMENTATION_URL
+    : process.env.NEXT_PUBLIC_TEMPORARY_DOCUMENTATION_URL;
 };
